6b047f274522a50b79a0096c5993707bf32ba24a
danigb <danigb@gmail.com>
2012-05-21 18:00:27 +0200

    Alguna notita

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class SessionsController < ApplicationController
   expose(:user) { User.new }
 
@@ -8,16 +9,16 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
+      redirect_to root_url, notice: "¡Bienvenida!"
     else
-      flash.now.alert = "Email or password is invalid"
+      flash.now.alert = "Correo o contraseña no válidos... Inténtalo de nuevo."
       render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out!"
+    redirect_to root_url, notice: "¡Hasta pronto!"
   end
 
   def enter
