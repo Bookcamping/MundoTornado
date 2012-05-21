707750d855cf37e99f3089d1d755e045168ec1c6
danigb <danigb@gmail.com>
2012-05-22 00:06:01 +0200

    Update page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class PagesController < ApplicationController
   respond_to :html
 
@@ -19,11 +20,23 @@ class PagesController < ApplicationController
     respond_with page
   end
 
+  def edit
+    authorize! :update, page
+    respond_with page
+  end
+
   def create
     page.user = current_user
     page.group = current_group
     authorize! :create, page
-    flash[:notice] = 'Lo tenemos!' if page.save
+    flash[:notice] = '¡Lo tenemos!' if page.save
+    respond_with page
+  end
+
+
+  def update
+    authorize! :update, page
+    flash[:notice] = '¡Ahora sí!' if page.update_attributes(params[:page])
     respond_with page
   end
 end
